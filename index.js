6f32c134229b78cd9991da3c0a800641f03e9e48
Kabir Singh <kabirsingh12b@gmail.com>
2024-10-13 02:11:06 +0530

    chore : basic socket implmentation

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
-const app = express();
 const http = require("http");
-const server = http.createServer(app);
+const { Server } = require("socket.io");
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("./public"));
+
+app.get("/", (res) => {
+  return res.sendFile("./public/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    // console.log("new user message ❗----------> ", message);
+    io.emit("message", message);
+  });
 });
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log("listening on http://localhost:3000");
 });
